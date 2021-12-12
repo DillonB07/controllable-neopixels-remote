586396c190fac4c61a8185ae546b308b191b25df
Dillon Barnes <dbarnes18@beaumontleys.leicester.sch.uk>
2021-12-12 19:34:43 +0000

    Update README

--- README.md
+++ README.md
@@ -1,14 +1,48 @@
+# Controllable Neopixels - Remote
+Strip - https://github.com/DillonB07/controllable-neopixels-strip/
+
+This remote uses the Math & Automatic for micro:bit found [here](https://thepihut.com/products/micro-touch-keyboard-math-automation-touch-keyboard-for-micro-bit) to take user input.
+
+Controls *(Menu 1)*:
+* 1 - Red
+* 2 - Green
+* 3 - Blue
+* 4 - Pink
+* 5 - Purple
+* 6 - Cyan
+* 7 - Red + Green alternating
+* 8 - Orange
+* 9 - Rainbow
+* 0 - Clear
+* + - Increase brightness
+* - - Decrease brightness
+* ÷ - Rotate pixels anti-clockwise
+* x - Rotate pixels clockwise
+* DF logo - Open menu two
+
+Controls *(Menu 2)*:
+* 1 - Continuously rotate pixels clockwise
+* 2 - Continuously rotate pixels anti-clockwise
+* 3 - Stop rotating pixels
+* 4 - Manual RGB input
+* 5 - Manual HSL input
+* + - Full brightness
+* - - Lowest brightness
+* DF logo - Return to main menu
+
+You can tell whether you're on menu 1 or 2 by looking at the screen. If there is an S you are on menu 2. If it's blank, you're on menu one.
+
+Strip - https://github.com/DillonB07/controllable-neopixels-strip/
+Remote - https://github.com/DillonB07/controllable-neopixels-remote/
+Demo (Bad quality video, sorry!) - https://youtu.be/cfV-Bsa7Wyg
+Math & Automatic - https://thepihut.com/products/micro-touch-keyboard-math-automation-touch-keyboard-for-micro-bit
+
+
+Generic template below
+---
 
 > Open this page at [https://dillonb07.github.io/controllable-neopixels-remote/](https://dillonb07.github.io/controllable-neopixels-remote/)
 
-## Use as Extension
-
-This repository can be added as an **extension** in MakeCode.
-
-* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
-* click on **New Project**
-* click on **Extensions** under the gearwheel menu
-* search for **https://github.com/dillonb07/controllable-neopixels-remote** and import
 
 ## Edit this project ![Build status badge](https://github.com/dillonb07/controllable-neopixels-remote/workflows/MakeCode/badge.svg)
 
--- main.ts
+++ main.ts
@@ -1,253 +1,132 @@
-keyboard.kbEvent(KeyValue.key4, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.key4, function my_function() {
+    
+    if (!menu) {
         radio.sendNumber(4)
         music.playTone(262, music.beat(BeatFraction.Eighth))
     } else {
         basic.showString("R")
-        while (keyboard.keyBasic() == -1) {
-        	
+        while (R1 == -1) {
+            R1 = keyboard.keyBasic()
         }
-        R1 = keyboard.keyBasic()
         basic.showNumber(R1)
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        R2 = keyboard.keyBasic()
-        basic.showNumber(R2)
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        R3 = keyboard.keyBasic()
-        basic.showNumber(R3)
-        R = 100 * R1 + (10 * R2 + R3)
-        basic.showString("G")
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        G1 = keyboard.keyBasic()
-        basic.showNumber(G1)
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        G2 = keyboard.keyBasic()
-        basic.showNumber(G2)
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        G3 = keyboard.keyBasic()
-        basic.showNumber(G3)
-        G = 100 * G1 + (10 * G2 + G3)
-        basic.showString("B")
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        B1 = keyboard.keyBasic()
-        basic.showNumber(B1)
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        B2 = keyboard.keyBasic()
-        basic.showNumber(B2)
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        B3 = keyboard.keyBasic()
-        basic.showNumber(B3)
-        B = 100 * B1 + (10 * B2 + B3)
-        radio.sendValue("R", R)
-        radio.sendValue("G", G)
-        radio.sendValue("B", B)
-        radio.sendValue("RGBready", 1)
-        basic.showString("S")
     }
+    
 })
-keyboard.kbEvent(KeyValue.key7, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.key7, function my_function2() {
+    if (!menu) {
         radio.sendNumber(7)
         music.playTone(262, music.beat(BeatFraction.Eighth))
     }
+    
 })
-radio.onReceivedNumber(function (receivedNumber) {
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
     radio.sendNumber(receivedNumber)
 })
-keyboard.kbEvent(KeyValue.key1, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.key1, function my_function3() {
+    if (!menu) {
         radio.sendNumber(1)
     } else {
         radio.sendString("S*")
     }
+    
     music.playTone(262, music.beat(BeatFraction.Quarter))
 })
-keyboard.kbEvent(KeyValue.keyminus, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.keyminus, function my_function4() {
+    if (!menu) {
         radio.sendString("-")
-    } else {
-        radio.sendString("S-")
         music.playTone(262, music.beat(BeatFraction.Eighth))
     }
+    
 })
-keyboard.kbEvent(KeyValue.key5, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.key5, function my_function5() {
+    if (!menu) {
         radio.sendNumber(5)
         music.playTone(262, music.beat(BeatFraction.Eighth))
-    } else {
-        basic.showString("H")
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        H1 = keyboard.keyBasic()
-        basic.showNumber(H1)
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        H2 = keyboard.keyBasic()
-        basic.showNumber(H2)
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        H3 = keyboard.keyBasic()
-        basic.showNumber(H3)
-        H = 100 * H1 + (10 * H2 + H3)
-        basic.showString("S")
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        S1 = keyboard.keyBasic()
-        basic.showNumber(S1)
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        S2 = keyboard.keyBasic()
-        basic.showNumber(S2)
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        S3 = keyboard.keyBasic()
-        basic.showNumber(S3)
-        S = 100 * S1 + (10 * S2 + S3)
-        basic.showString("L")
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        L1 = keyboard.keyBasic()
-        basic.showNumber(L1)
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        L2 = keyboard.keyBasic()
-        basic.showNumber(L2)
-        while (keyboard.keyBasic() == -1) {
-        	
-        }
-        L3 = keyboard.keyBasic()
-        basic.showNumber(L3)
-        L = 100 * L1 + (10 * L2 + L3)
-        radio.sendValue("H", H)
-        radio.sendValue("S", S)
-        radio.sendValue("L", L)
-        radio.sendValue("HSLready", 1)
-        basic.showString("S")
     }
+    
 })
-keyboard.kbEvent(KeyValue.keydiv, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.keydiv, function my_function6() {
+    
+    if (!menu) {
         radio.sendString("/")
         waiting = true
         music.playTone(262, music.beat(BeatFraction.Eighth))
     }
+    
 })
-keyboard.kbEvent(KeyValue.key9, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.key9, function my_function7() {
+    if (!menu) {
         radio.sendNumber(9)
         music.playTone(262, music.beat(BeatFraction.Eighth))
     }
+    
 })
-keyboard.kbEvent(KeyValue.key8, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.key8, function my_function8() {
+    if (!menu) {
         radio.sendNumber(8)
         music.playTone(262, music.beat(BeatFraction.Eighth))
     }
+    
 })
-keyboard.kbEvent(KeyValue.key0, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.key0, function my_function9() {
+    if (!menu) {
         radio.sendNumber(0)
         music.playTone(262, music.beat(BeatFraction.Eighth))
     }
+    
 })
-keyboard.kbEvent(KeyValue.key6, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.key6, function my_function10() {
+    if (!menu) {
         radio.sendNumber(6)
         music.playTone(262, music.beat(BeatFraction.Eighth))
-    } else {
-        radio.sendString("S6")
-        music.playTone(262, music.beat(BeatFraction.Eighth))
     }
+    
 })
-radio.onReceivedString(function (receivedString) {
+radio.onReceivedString(function on_received_string(receivedString: string) {
     radio.sendString(receivedString)
 })
-keyboard.kbEvent(KeyValue.keymul, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.keymul, function my_function11() {
+    if (!menu) {
         radio.sendString("*")
         music.playTone(262, music.beat(BeatFraction.Eighth))
     }
+    
 })
-keyboard.kbEvent(KeyValue.key3, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.key3, function my_function12() {
+    if (!menu) {
         radio.sendNumber(3)
     } else {
         radio.sendString("SR")
     }
+    
     music.playTone(262, music.beat(BeatFraction.Eighth))
 })
-keyboard.kbEvent(KeyValue.keydf, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.keydf, function my_function13() {
+    
+    if (!menu) {
         menu = true
         basic.showString("S")
     } else {
         menu = false
-        basic.clearScreen()
     }
+    
 })
-keyboard.kbEvent(KeyValue.keyplus, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.keyplus, function my_function14() {
+    if (!menu) {
         radio.sendString("+")
-    } else {
-        radio.sendString("S+")
         music.playTone(262, music.beat(BeatFraction.Eighth))
     }
+    
 })
-keyboard.kbEvent(KeyValue.key2, function () {
-    if (!(menu)) {
+keyboard.kbEvent(KeyValue.key2, function my_function15() {
+    if (!menu) {
         radio.sendNumber(2)
     } else {
         radio.sendString("S/")
     }
+    
     music.playTone(262, music.beat(BeatFraction.Eighth))
 })
-let L = 0
-let L3 = 0
-let L2 = 0
-let L1 = 0
-let S = 0
-let S3 = 0
-let S2 = 0
-let S1 = 0
-let H = 0
-let H3 = 0
-let H2 = 0
-let H1 = 0
-let B = 0
-let B3 = 0
-let B2 = 0
-let B1 = 0
-let G = 0
-let G3 = 0
-let G2 = 0
-let G1 = 0
-let R = 0
-let R3 = 0
-let R2 = 0
 let R1 = 0
 let waiting = false
 let menu = false
@@ -266,7 +145,8 @@ keyboard.setIndexColor(4, keyboard.rgb(255, 0, 123))
 keyboard.setIndexColor(5, keyboard.rgb(149, 0, 255))
 keyboard.setIndexColor(6, keyboard.rgb(0, 255, 251))
 keyboard.setIndexColor(8, keyboard.rgb(255, 127, 0))
-loops.everyInterval(1000, function () {
+loops.everyInterval(1000, function on_every_interval() {
+    
     if (led_7 == "G") {
         keyboard.setIndexColor(7, keyboard.rgb(255, 0, 0))
         led_7 = "R"
@@ -274,6 +154,7 @@ loops.everyInterval(1000, function () {
         keyboard.setIndexColor(7, keyboard.rgb(0, 255, 0))
         led_7 = "G"
     }
+    
     if (led_9 == "V") {
         keyboard.setIndexColor(9, keyboard.rgb(255, 0, 0))
         led_9 = "R"
@@ -296,4 +177,5 @@ loops.everyInterval(1000, function () {
         keyboard.setIndexColor(9, keyboard.rgb(148, 0, 211))
         led_9 = "V"
     }
+    
 })
